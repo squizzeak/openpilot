9e3d13b897cf46cb26e61551090aebd738f31b9d
Todd Hendricks <todd@hendricks.cc>
2024-05-27 12:29:21 -0500

    Port some lateral tuning from jvePilot

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -106,10 +106,10 @@ class CarControllerParams:
       self.STEER_DELTA_DOWN = 6
       self.STEER_MAX = 350  # EPS allows more, up to 350?
     else:
-      self.STEER_DELTA_UP = 3
-      self.STEER_DELTA_DOWN = 3
-      self.STEER_MAX = 261  # higher than this faults the EPS
-
+      self.STEER_STEP = 1  # 100 Hz
+      self.STEER_DELTA_UP = 6
+      self.STEER_DELTA_DOWN = 6
+      self.STEER_MAX = 261
 
 STEER_THRESHOLD = 120
 
