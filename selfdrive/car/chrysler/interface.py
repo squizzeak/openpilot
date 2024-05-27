692cf1c9a5982b506d8d23c7fd227b5e6f644717
Todd Hendricks <todd@hendricks.cc>
2024-05-27 12:29:21 -0500

    Port some lateral tuning from jvePilot

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -46,10 +46,10 @@ class CarInterface(CarInterfaceBase):
     elif candidate in (CAR.JEEP_GRAND_CHEROKEE, CAR.JEEP_GRAND_CHEROKEE_2019):
       ret.steerActuatorDelay = 0.2
 
-      ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
-      ret.lateralTuning.pid.kf = 0.00006
+      # ret.lateralTuning.init('pid')
+      # ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
+      # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
+      # ret.lateralTuning.pid.kf = 0.00006
       ret.wheelSpeedFactor = 1.035
 
     # Ram
