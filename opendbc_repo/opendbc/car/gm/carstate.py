370e92f439ce7a40f62b8eb2e8333d29495ff08c
James <91348155+FrogAi@users.noreply.github.com>
2025-10-07 09:37:20 -0700

    Acceleration/Deceleration Profiles

--- opendbc_repo/opendbc/car/gm/carstate.py
+++ opendbc_repo/opendbc/car/gm/carstate.py
@@ -185,6 +185,9 @@ class CarState(CarStateBase):
     # FrogPilot variables
     fp_ret = custom.FrogPilotCarState.new_message()
 
+    if "SportMode" in pt_cp.vl:
+      fp_ret.sportGear = pt_cp.vl["SportMode"]["SportMode"] == 1
+
     return ret, fp_ret
 
   @staticmethod
--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -65,6 +65,7 @@ class CarState(CarStateBase):
     self.params = CarControllerParams(CP)
 
     # FrogPilot variables
+    self.drive_mode = 0
 
   def recent_button_interaction(self) -> bool:
     # On some newer model years, the CANCEL button acts as a pause/resume button based on the PCM state
@@ -301,6 +302,14 @@ class CarState(CarStateBase):
 
     # FrogPilot variables
     fp_ret = custom.FrogPilotCarState.new_message()
+
+    if "DRIVE_MODE" in cp.vl:
+      drive_mode = cp.vl["DRIVE_MODE"]["DRIVE_MODE2"]
+      if drive_mode != 0 and drive_mode != self.drive_mode:
+        self.drive_mode = drive_mode
+
+    fp_ret.ecoGear = self.drive_mode == 2
+    fp_ret.sportGear = self.drive_mode == 3
 
     return ret, fp_ret
 
