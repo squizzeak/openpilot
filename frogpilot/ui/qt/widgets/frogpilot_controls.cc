42aef3ddfae4048a17a0dc42465e700079e64027
James <91348155+FrogAi@users.noreply.github.com>
2025-10-11 19:09:00 -0700

    FrogPilot panel

--- frogpilot/ui/qt/widgets/frogpilot_controls.cc
+++ frogpilot/ui/qt/widgets/frogpilot_controls.cc
@@ -2,34 +2,38 @@
 
 #include "frogpilot/ui/frogpilot_ui.h"
 
+bool FrogPilotConfirmationDialog::toggleReboot(QWidget *parent) {
+  ConfirmationDialog d(tr("Reboot required to take effect."), tr("Reboot Now"), tr("Reboot Later"), false, parent);
+  return d.exec();
+}
+
+bool FrogPilotConfirmationDialog::yesorno(const QString &prompt_text, QWidget *parent) {
+  ConfirmationDialog d(prompt_text, tr("Yes"), tr("No"), false, parent);
+  return d.exec();
+}
+
 void clearMovie(QSharedPointer<QMovie> &movie, QWidget *parent) {
   if (movie) {
     QObject::disconnect(movie.data(), &QMovie::frameChanged, parent, nullptr);
     movie->stop();
-    movie.clear();
+    movie.reset();
   }
 }
 
 void loadGif(const QString &gifPath, QSharedPointer<QMovie> &movie, const QSize &size, QWidget *parent) {
   clearMovie(movie, parent);
 
-  if (QFileInfo::exists(gifPath)) {
-    movie = QSharedPointer<QMovie>::create(gifPath, QByteArray(), parent);
-    movie->setCacheMode(QMovie::CacheAll);
-    movie->setScaledSize(size);
+  movie = QSharedPointer<QMovie>::create(gifPath, QByteArray(), parent);
+  movie->setCacheMode(QMovie::CacheAll);
+  movie->setScaledSize(size);
 
-    QObject::connect(movie.data(), &QMovie::frameChanged, parent, [parent](int) {
-      if (parent) {
-        parent->update();
-      }
-    }, Qt::UniqueConnection);
+  QObject::connect(movie.data(), &QMovie::frameChanged, parent, [parent]() {
+    if (parent->isVisible()) {
+      parent->update();
+    }
+  }, Qt::UniqueConnection);
 
-    movie->start();
-  }
-
-  if (parent) {
-    parent->update();
-  }
+  movie->start();
 }
 
 void loadImage(const QString &basePath, QPixmap &pixmap, QSharedPointer<QMovie> &movie, const QSize &size, QWidget *parent) {
@@ -41,9 +45,17 @@ void loadImage(const QString &basePath, QPixmap &pixmap, QSharedPointer<QMovie> 
   } else {
     clearMovie(movie, parent);
     pixmap = QPixmap(basePath + ".png").scaled(size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+  }
 
-    if (parent) {
-      parent->update();
+  parent->update();
+}
+
+void openDescriptions(bool forceOpenDescriptions, std::map<QString, AbstractControl*> toggles) {
+  if (forceOpenDescriptions) {
+    for (auto &[key, toggle] : toggles) {
+      if (key != "CESpeed") {
+        toggle->showDescription();
+      }
     }
   }
 }
