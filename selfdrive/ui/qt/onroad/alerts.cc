7221e3ae739e3e1bd00908aebb9d7a4112028811
James <91348155+FrogAi@users.noreply.github.com>
2025-11-16 08:41:19 -0700

    Hide Alerts

--- selfdrive/ui/qt/onroad/alerts.cc
+++ selfdrive/ui/qt/onroad/alerts.cc
@@ -8,8 +8,12 @@
 void OnroadAlerts::updateState(const UIState &s, const FrogPilotUIState &fs) {
   Alert a = getAlert(*(s.sm), *(fs.sm), s.scene.started_frame, fs.frogpilot_scene.frogpilot_toggles);
   if (!alert.equal(a)) {
-    alert = a;
-    update();
+    if (alert.status == cereal::SelfdriveState::AlertStatus::NORMAL && fs.frogpilot_scene.frogpilot_toggles.value("hide_alerts").toBool()) {
+      clear();
+    } else {
+      alert = a;
+      update();
+    }
   }
 
   // FrogPilot variables
